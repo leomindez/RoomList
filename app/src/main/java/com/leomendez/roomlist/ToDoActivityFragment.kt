dacfb6297b87111ea6b4c7800b017c5d44ea8c45
Leo Mendez <leomendez@MacBook-Pro-de-Leo.local>
2017-05-20 12:02:57 -0500

    NEW: Added kotlin version

---
+++ app/src/main/java/com/leomendez/roomlist/ToDoActivityFragment.kt
@@ -0,0 +1,18 @@
+package com.leomendez.roomlist
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class ToDoActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_to_do, container, false)
+    }
+}
