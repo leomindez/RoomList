ca81a4815b94195706671b36d687571fec5819c3
Leo Mendez <leomendez@MacBook-Pro-de-Leo.local>
2017-05-23 23:22:41 -0500

    NEW: Added add svg icon

---
+++ app/src/main/java/com/leomendez/roomlist/view/AddToDoActivity.kt
@@ -0,0 +1,14 @@
+package com.leomendez.roomlist.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.leomendez.roomlist.R
+
+class AddToDoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_to_do)
+    }
+}
